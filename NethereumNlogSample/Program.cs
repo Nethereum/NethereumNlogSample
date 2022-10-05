3c9b2945c80bee4ffc8e19ee3c1951e9e727f0d0
Juan Blanco <juanfranblanco@hotmail.com>
2022-10-05 12:39:51 +0100

    Add project files.

---
+++ NethereumNlogSample/Program.cs
@@ -0,0 +1,11 @@
+﻿
+using Microsoft.Extensions.Logging;
+using Nethereum.Web3;
+using NLog.Extensions.Logging;
+
+var logger = LoggerFactory.Create(builder => builder.AddNLog()).CreateLogger<Program>();
+logger.LogInformation("hello info!");
+logger.LogTrace("hello trace!");
+var web3 = new Web3("http://localhost:8545", logger);
+var blocknumber = await web3.Eth.Blocks.GetBlockNumber.SendRequestAsync();
+Console.ReadLine();
